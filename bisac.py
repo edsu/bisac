53a4b11846a4a7e9e4d8bd2926e6e08ae92c8703
Ed Summers <ehs@pobox.com>
2011-11-17 07:47:02 -0500

    attempting to add related

--- bisac.py
+++ bisac.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import json
 import logging
 
@@ -12,24 +13,35 @@ def concepts():
     """
     concepts = {}
     alt_labels = {}
+    relateds = {}
     for url in urls():
         logging.info("parsing %s", url)
         results = assemble(url)
         concepts.update(results[0])
         alt_labels.update(results[1])
+        relateds.update(results[2])
 
     # now that we have all of the concepts we can add the alt labels
     for pref_label in alt_labels.keys(): 
         if concepts.has_key(pref_label):
-            concepts[pref_label]['alt_labels'] = alt_labels[pref_label]
+            concepts[pref_label]['alt_label'] = alt_labels[pref_label]
         else:
             logging.warn("missing pref label in alt_labels: %s", pref_label)
+
+    # and related labels
+    for pref_label in relateds.keys():
+        if concepts.has_key(pref_label):
+            for related in relateds[pref_label]:
+                concepts[pref_label]['related'].append(related)
+        else:
+            logging.warn("missing pref label in relateds: %s", pref_label)
 
     open("bisac.json", "w").write(json.dumps(concepts, indent=2))
 
 def assemble(url):
     concepts = {}
     alt_labels = {}
+    relateds = {}
     for term in parse(url):
         logging.info("got term: %s", term)
         term_type = term.pop(0)
@@ -37,17 +49,24 @@ def assemble(url):
             notation, pref_label = term
             concepts[pref_label] = {"notation": notation,
                                     "pref_label": pref_label,
-                                    "alt_labels": []}
+                                    "alt_label": [],
+                                    "related": []}
         elif term_type == "ALT_LABEL":
             alt_label = term.pop(0)
             for pref_label in term:
-                if alt_labels.has_key(pref_label):
-                    alt_labels[pref_label].append(alt_label)
-                else:
-                    alt_labels[pref_label] = [alt_label]
-        else:
-            raise "unknown term type"
-    return concepts, alt_labels
+                add(alt_labels, pref_label, alt_label)
+        elif term_type == "RELATED":
+            pref_label = term.pop(0)
+            for related in term:
+                add(relateds, pref_label, related)
+
+    return concepts, alt_labels, relateds
+
+def add(d, k, v):
+    if d.has_key(k):
+        d[k].append(v)
+    else:
+        d[k] = [v]
 
 def parse(url):
     """A generator that returns concepts and alt labels as terms from 
@@ -62,11 +81,16 @@ def parse(url):
         notation = cells[0].text
         labels = extract_labels(cells[1])
 
-        if notation and len(labels) == 1:
+        if not notation:
+            for see_also in labels[1:]:
+                yield ['ALT_LABEL', labels[0], see_also]
+        elif notation and len(labels) == 1:
             yield ['CONCEPT', notation, labels[0]]
-
-        for see_also in labels[1:]:
-            yield ['ALT_LABEL', labels[0], see_also]
+        elif notation and len(labels) > 1:
+            for related in labels[1:]:
+                yield['RELATED', labels[0], related]
+        else:
+            logging.error("unknown term type")
 
 def urls():
     url = "http://www.bisg.org/what-we-do-0-136-bisac-subject-headings-list-major-subjects.php"
@@ -75,7 +99,7 @@ def urls():
         yield urljoin(url, a.attrib['href'])
 
 def format_label(s):
-    s = s.lower().strip().strip(' *')
+    s = s.lower().strip().strip(' *').strip(')')
     parts = s.split(' ')
     parts = [p.capitalize() for p in parts]
     return ' '.join(parts)
@@ -88,8 +112,6 @@ def extract_labels(e):
     into:
 
     ["Antiques & Collectibles / Hummels", "Antiques & Collectibles / Figurines", "Antiques & Collectibles / Popular Culture"]
-
-    But look Ma, no regexes!
     """
     from_label = format_label(e.text)
     main = hierarchy(from_label)[0]
@@ -97,11 +119,9 @@ def extract_labels(e):
     chain = [from_label]
     for p in e:
         if p.tail == None:
-            # for degenerative cases like:
-            # <td>PHILOSOPHY / Western <i>see specific headings in this section</i></td>
             continue
         see_label = format_label(p.tail)
-        if (len(hierarchy(see_label)) == 1):
+        if not re.match('^[A-Z]+ /.*$', p.tail):
             see_label = " / ".join([main, see_label])
         chain.append(see_label)
     return chain
